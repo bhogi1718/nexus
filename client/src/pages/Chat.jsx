2e05a101175f01ad90f21d7e09c95c14431685d4
Charan Tej <bhogicharantej@gmail.com>
2026-07-22 00:37:13 +0530

    hey

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1239,7 +1239,7 @@ export const Chat = () => {
               </form>
             </>
           ) : (
-            <div className="flex-1 flex items-center justify-center text-gray-500">
+            <div className="hidden lg:flex flex-1 items-center justify-center text-gray-500">
               Select a conversation to start chatting
             </div>
           )}
